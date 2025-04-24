24919c5ca39a72b2db74fa9f801095081f4f5812
sueKK <125456919+sueKK@users.noreply.github.com>
2025-04-25 00:23:18 +0300

    add date dependencies

--- src/components/BookingForm.jsx
+++ src/components/BookingForm.jsx
@@ -1,7 +1,8 @@
-import { db } from "./firebase"; // adjust path if needed
+import { db } from "/firebase"; // adjust path if needed
 import { collection, addDoc } from "firebase/firestore";
 import { div } from "framer-motion/client";
 import { useState } from "react";
+import { motion } from "motion/react";
 
 export default function BookingSection() {
   const [date, setDate] = useState(null);
@@ -25,10 +26,22 @@ export default function BookingSection() {
   };
 
   return (
-    <div>
-    <form onSubmit={handleSubmit} className="your-form-classes">
-      {/* your fields here */}
-    </form>
-    </div>
+    <motion.div
+    initial={{opacity:0, x:-200}}
+    transition={{duration:1}}
+    whileInView={{opacity:1, x:0}}
+    viewport={{once: true}}
+
+         className='text-center p-6 py-20 lg:px-32 w-full overflow-hidden' id='Contact'>
+         <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center'>Book your stay<span className='underline 
+        underline-offset-4  decoration-1 under font-light'> with Us</span></h1>
+        <p className='text-center text-gray-500 mb-12 max-w-80
+        mx-auto'>Ready to make a move? Book your stay with us</p>
+        <div>
+         <form onSubmit={handleSubmit} className="your-form-classes">
+          {/* your fields here */}
+         </form>
+         </div>
+    </motion.div>
   );
 }
