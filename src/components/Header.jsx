72637812352f70934db572d74dca90037ebe3f08
sueKK <125456919+sueKK@users.noreply.github.com>
2025-05-03 21:50:56 +0300

    Correct Home page responce

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,7 +17,7 @@ function Header() {
         className='container text-center mx-auto py-4 px-6 md:px-20 lg:px-32 text-white'>
             <h2 className='text-5xl sm:text-6xl md:text-[82px] inline-block
             max-w-3xl font-semibold pt-20'>A Peaceful Escape in Full Bloom</h2>
-            <div className='space-x-6 mt-16'>
+            <div className='flex flex-col sm:flex-row gap-4 sm:gap-6 items-center justify-center mt-6 '>
                 <a href="#Gallery" className=' hover:bg-assent border border-white px-8 py-3 rounded'>Our Gallery</a>
                 <a href="#booking" className='bg-assent px-8 py-3 rounded'>Book Your Stay</a>
             </div>
